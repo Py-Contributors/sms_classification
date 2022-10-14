c8f2f75a755253025907bc1583321b2b3be2cce0
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-10-14 23:32:51 +0530

    adding openapi interactive api docs

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
-from flask import Flask, request, jsonify
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from pydantic import BaseModel
+
 from prediction import predict_txt
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def home():
-    return 'Hello World!'
+class Item(BaseModel):
+    text: str
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # get text data from post request
-    text = request.form['text']
+@app.get('/')
+async def home():
+    return {'API' : 'SMS Classification API'}
+
+@app.post('/predict')
+async def predict(data: Item):
+    text = data.text
     output = predict_txt(text)
+    return {'status' : 'success', 'prediction' : output}
 
-    return jsonify({'prediction': output})
-
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="SMS Classification API",
+        version="1.0.0",
+        description="This is a simple API for SMS Classification",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 def main():
-    app.run(debug=True)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
 
 if __name__ == '__main__':
     main()
